e279c3235558112e6164fbf6d50c669ff7617aa9
Vitali Filipchuk <vitali.filipchuk@gmail.com>
2021-12-05 21:55:19 +0200

    started addBlog component

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,11 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000;
 }
 
 #nav {
---
+++ src/components/addBlog.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="add-blog">
+        <h2>Add a New Blog Post</h2>
+        <form action="">
+            <label>Blog Title:</label>
+            <input type="text" required>
+            <label>Blog Text:</label>
+            <textarea></textarea>
+        </form>
+        <div id="preview">
+            <h3>Preview Blog</h3>
+            <p>Blog Title:</p>
+            <p>Blog Text:</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+#add-blog {
+    max-width: 480px;
+    margin: 30px auto;
+}
+
+#add-blog * {
+    box-sizing: border-box;
+}
+
+label {
+    display: block;
+    text-align: left;
+    margin: 10px 0;
+}
+
+input[type="text"],
+textarea {
+    width: 100%;
+    border-radius: 5px;
+    border: 1px solid #333;
+    font-size: 16px;
+    line-height: 1.5;
+    padding: 6px 12px;
+}
+
+textarea {
+    resize: none;
+    min-height: 138px;
+}
+</style>
